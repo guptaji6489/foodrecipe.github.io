584c1b65b6d12356955f6b15b16b62448a6f405a
guptaji6489 <guptaji6489@gmail.com>
2020-07-08 18:28:23 +0530

    done

---
+++ precache-manifest.1f62fd16060477d3f20bb1d88263fdfe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49b3bf377b6b370ba6c8cf72323e641f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2ba5e3a9fdef26aa4af7",
+    "url": "/static/css/main.3eb15309.chunk.css"
+  },
+  {
+    "revision": "77e1a6519c3c500f1461",
+    "url": "/static/js/2.3fff89dd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3fff89dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ba5e3a9fdef26aa4af7",
+    "url": "/static/js/main.9d050bb7.chunk.js"
+  },
+  {
+    "revision": "d3f6128f987bfb5a3cbd",
+    "url": "/static/js/runtime-main.620353b0.js"
+  }
+]);
